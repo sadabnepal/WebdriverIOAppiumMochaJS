18b1836b461aacb95658f1f605bf2b762a885de0
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-01-22 11:36:24 +0530

    created constants

---
+++ test/pages/static/constants.js
@@ -0,0 +1,11 @@
+module.exports = {
+    APP_PACKAGE: "io.appium.android.apis",
+    APP_HEADER: "API Demos",
+    COMMAND_TWO_POPUP_MSG: "You selected: 1 , Command two",
+    MENU_ITEMS: [
+        'API Demos', 'Accessibility', 'Animation',
+        'App', 'Content', 'Graphics',
+        'Media', 'NFC', 'OS',
+        'Preference', 'Text', 'Views'
+    ]
+}
